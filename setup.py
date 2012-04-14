17a57e9705651d00c4b3454f7bb63b89fc7d773a
Luca Fabbri <luca@keul.it>
2012-04-14 17:08:58 +0200

    I like to see Plone with capital letter

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ version = '1.0a4'
 
 setup(name='collective.wtforms',
       version=version,
-      description="plone wtforms integration",
+      description="Plone wtforms integration",
       long_description=open("README.txt").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from
