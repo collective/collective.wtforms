3027b3ebf4a622ba6349465c9acd662252858f45
Luca Fabbri <luca@keul.it>
2012-04-21 15:32:34 +0200

    enabled form tabbing and added native WTForms internal translations

--- collective/wtforms/locales/runi18ndude.sh
+++ collective/wtforms/locales/runi18ndude.sh
@@ -2,5 +2,5 @@
 
 DOMAIN='collective.wtforms'
 
-i18ndude rebuild-pot --pot ${DOMAIN}.pot --create ${DOMAIN} ..
+i18ndude rebuild-pot --pot ${DOMAIN}.pot --merge wtforms.pot --create ${DOMAIN} ..
 i18ndude sync --pot ${DOMAIN}.pot ./*/LC_MESSAGES/${DOMAIN}.po
